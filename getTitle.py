f8c713cd9ebfc343c47a060851f4d602cd3d7dbf
sp35000 <sp35000@yahoo.com.br>
2022-11-17 08:39:01 -0300

    feat: QR Code generator added to webFlask

--- getTitle.py
+++ getTitle.py
@@ -7,4 +7,5 @@ url=(sys.argv[1])
 #url="https://www.swissinfo.ch/por/a-su%C3%AD%C3%A7a-defenderia-o-liechtenstein-no-caso-de-um-ataque-militar-/47798482"
 page = requests.get(url)
 soup = BeautifulSoup(page.text, 'html.parser')
-print(soup.html.head.title.text).encode('ascii', 'ignore').decode('ascii')
+#print(soup.html.head.title.text).encode('utf-16').replace("'", "\"").replace(chr(0xef),"").replace(chr(0xbd),"").replace(chr(0xbf),"")
+print(soup.html.head.title.text).encode('utf-16').replace("'", "\"")
--- webFlask/webFlask.py
+++ webFlask/webFlask.py
@@ -1,13 +1,26 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -- coding: utf-8
-from flask import Flask
+#import qrcode, base64, io
+from flask import Flask, request, render_template, flash
+from flask_qrcode import QRcode
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return '''
-    <h1>Hello world</h1>
-    <h2>Aqui temos uma tag h2</h2>
-    <h3>E aqui uma tag h3</h3>
-    <p>E aqui um parágrafo</p>
-    '''
-app.run()
+qrcode = QRcode(app)
+# echo -n "Ckrops-WebQRCodeGenerator-20221114"|sha512sum
+app.config['SECRET_KEY'] = '7680ee6d5b19f6e01a09148cc5313392d5caaa248329d1f5330920ae29d8b15e34723c87fe255acd277ff83998671335911e89275b3bf0129fdb078e1a3bca3a'
+@app.route('/', methods=['POST', 'GET'])
+def qrcodegen():
+ error = None
+ answer = None
+ if request.method == 'POST':
+  answer = (request.form['text'])
+#  rawBytes = io.BytesIO()
+#  img.save(rawBytes, "JPEG")
+#  img_base64 = base64.b64encode(rawBytes.read())
+  if (request.form['text']):
+   flash(answer)
+  else:
+   error = 'Invalid text'
+   # the code below is executed if the request method
+   # was GET or the credentials were invalid
+ return render_template('qrcode.html', error=error, answer=answer)
+app.run(host='0.0.0.0',port=2000)
